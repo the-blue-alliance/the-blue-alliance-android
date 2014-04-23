4b73486b920f846185eb5156350ff5543fde188b
Phil Lopreiato <plnyyanks@gmail.com>
2014-04-23 17:43:24 -0400

    better award layout

--- android/src/main/java/com/thebluealliance/androidclient/background/PopulateEventAwards.java
+++ android/src/main/java/com/thebluealliance/androidclient/background/PopulateEventAwards.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TableLayout;
 
 import com.thebluealliance.androidclient.R;
+import com.thebluealliance.androidclient.adapters.ListViewAdapter;
 import com.thebluealliance.androidclient.datatypes.AwardListElement;
+import com.thebluealliance.androidclient.datatypes.ListItem;
 
 import java.util.ArrayList;
 
@@ -19,7 +22,9 @@ public class PopulateEventAwards extends AsyncTask<String,Void,Void> {
     private Activity activity;
     private View view;
     private String eventKey;
-    private ArrayList<AwardListElement> awards;
+    private ArrayList<ListItem> awards;
+    private ArrayList<String> keys;
+    private ListViewAdapter adapter;
 
     public PopulateEventAwards(Activity activity, View view){
         this.activity = activity;
@@ -30,25 +35,24 @@ public class PopulateEventAwards extends AsyncTask<String,Void,Void> {
     protected Void doInBackground(String... params) {
         eventKey = params[0];
 
-        awards = new ArrayList<AwardListElement>();
+        awards = new ArrayList<ListItem>();
+        keys = new ArrayList<String>();
 
         //add some temp data
-        awards.add(new AwardListElement("frc1311","Regional Chairman's Award","1311"));
-        awards.add(new AwardListElement("frc2974", "Engineering Inspiration Award", "2974"));
-        awards.add(new AwardListElement("frc4965", "Rookie All Star", "4965"));
-        awards.add(new AwardListElement("frc4551", "Woodie Flowers Finalist Award", "James Bryan\n(4551)"));
+        keys.add("frc1311");    awards.add(new AwardListElement("frc1311","Regional Chairman's Award","1311"));
+        keys.add("frc2974");    awards.add(new AwardListElement("frc2974", "Engineering Inspiration Award", "2974"));
+        keys.add("frc4965");    awards.add(new AwardListElement("frc4965", "Rookie All Star", "4965"));
+        keys.add("frc4551");    awards.add(new AwardListElement("frc4551", "Woodie Flowers Finalist Award", "James Bryan\n(4551)"));
+
+        adapter = new ListViewAdapter(activity,awards,keys);
 
         return null;
     }
 
     @Override
     protected void onPostExecute(Void aVoid) {
-        TableLayout rankings = (TableLayout)view.findViewById(R.id.event_awards);
-        LayoutInflater inflater = activity.getLayoutInflater();
-        for(AwardListElement award: awards){
-            rankings.addView(award.getView(inflater,null));
-        }
-
+        ListView rankings = (ListView)view.findViewById(R.id.event_awards);
+        rankings.setAdapter(adapter);
     }
 
 }
