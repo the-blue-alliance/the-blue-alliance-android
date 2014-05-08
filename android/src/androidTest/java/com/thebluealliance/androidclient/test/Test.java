707aff7080b12c58ebfbcf5380141ba4707beaa6
Phil Lopreiato <plnyyanks@gmail.com>
2014-05-07 22:25:45 -0400

    Add unit test framework and a basic test case

---
+++ android/src/androidTest/java/com/thebluealliance/androidclient/test/Test.java
@@ -0,0 +1,14 @@
+package com.thebluealliance.androidclient.test;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import junit.framework.TestCase;
+
+public class Test extends TestCase {
+
+    @SmallTest
+    public void testBasicStuff() {
+        assertEquals(true,true);
+    }
+
+}
