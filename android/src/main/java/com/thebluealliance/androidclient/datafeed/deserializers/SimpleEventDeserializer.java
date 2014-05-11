97840352f6846acb26fc966e196ccd63edd40095
Nathan Walters <nwalters512@gmail.com>
2014-05-11 18:27:39 -0500

    Fix for event deserializers breaking when no "short_name" was specified

--- android/src/main/java/com/thebluealliance/androidclient/datafeed/deserializers/SimpleEventDeserializer.java
+++ android/src/main/java/com/thebluealliance/androidclient/datafeed/deserializers/SimpleEventDeserializer.java
@@ -10,24 +10,30 @@ import com.thebluealliance.androidclient.models.SimpleEvent;
 import java.lang.reflect.Type;
 
 
-public class SimpleEventDeserializer implements JsonDeserializer<SimpleEvent>{
-	
-	@Override
-	public SimpleEvent deserialize(final JsonElement json, Type typeOf, JsonDeserializationContext context) throws JsonParseException {
-		final JsonObject object = json.getAsJsonObject();
-		final SimpleEvent event = new SimpleEvent();
-		
-		event.setEventKey(object.get("key").getAsString());
-		event.setEventName(object.get("name").getAsString());
-		event.setLocation(object.get("location").getAsString());
-		event.setEventType(object.get("event_type").getAsInt());
-		event.setEventDistrict(""); /* NOT IMPLEMENTED IN API. Modify whenever it is... */
-		event.setStartDate(object.get("start_date").getAsString());
-		event.setEndDate(object.get("end_date").getAsString());
-		event.setOfficial(object.get("official").getAsBoolean());
-        event.setShortName(object.get("short_name").getAsString());
-		event.setLastUpdated(System.currentTimeMillis());
+public class SimpleEventDeserializer implements JsonDeserializer<SimpleEvent> {
 
-		return event;
-	}
+    @Override
+    public SimpleEvent deserialize(final JsonElement json, Type typeOf, JsonDeserializationContext context) throws JsonParseException {
+        final JsonObject object = json.getAsJsonObject();
+        final SimpleEvent event = new SimpleEvent();
+
+        event.setEventKey(object.get("key").getAsString());
+        event.setEventName(object.get("name").getAsString());
+        event.setLocation(object.get("location").getAsString());
+        event.setEventType(object.get("event_type").getAsInt());
+        event.setEventDistrict(""); /* NOT IMPLEMENTED IN API. Modify whenever it is... */
+        event.setStartDate(object.get("start_date").getAsString());
+        event.setEndDate(object.get("end_date").getAsString());
+        event.setOfficial(object.get("official").getAsBoolean());
+        // "short_name" is not a required field in the API response.
+        // If it is null, simply use the event name as the short name
+        if (object.get("short_name").isJsonNull()) {
+            event.setShortName(object.get("name").getAsString());
+        } else {
+            event.setShortName(object.get("short_name").getAsString());
+        }
+        event.setLastUpdated(System.currentTimeMillis());
+
+        return event;
+    }
 }
