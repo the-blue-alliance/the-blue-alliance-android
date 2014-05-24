ada2e5861edf2c0ec9822e660dfb9bf34254ccf2
Bryce Matsuda <iexist4jcore@gmail.com>
2014-05-23 14:56:20 -1000

    Added size check when getting OPRs, since some events have OPR keys with nothing in it. This resulted in a blank "Top OPRs:" being shown. This also resulted in some events not showing "Top OPRs:" despite having stats.

--- android/src/main/java/com/thebluealliance/androidclient/background/event/PopulateEventInfo.java
+++ android/src/main/java/com/thebluealliance/androidclient/background/event/PopulateEventInfo.java
@@ -102,7 +102,9 @@ public class PopulateEventInfo extends AsyncTask<String, String, APIResponse.COD
                 try {
                     APIResponse<JsonObject> statsResponse = DataManager.getEventStats(activity, eventKey);
                     ArrayList<Map.Entry<String, JsonElement>> opr = new ArrayList<>();
-                    if(statsResponse.getData().has("oprs")) {
+                    if(statsResponse.getData().has("oprs") &&
+                       statsResponse.getData().get("oprs").getAsJsonObject().entrySet().size() > 0) {
+                        // ^ Make sure we actually have OPRs in our set!
                         opr.addAll(statsResponse.getData().get("oprs").getAsJsonObject().entrySet());
                         Collections.sort(opr, new TeamSortByOPRComparator());
                         String statsString = "";
