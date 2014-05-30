9a196892233abbcd22c9159756ef3d4e892e3e03
Phil Lopreiato <plnyyanks@gmail.com>
2014-05-29 20:34:48 -0400

    Add link to Github release (incl. changelog) in Settings activity

--- android/src/main/java/com/thebluealliance/androidclient/activities/SettingsActivity.java
+++ android/src/main/java/com/thebluealliance/androidclient/activities/SettingsActivity.java
@@ -38,6 +38,14 @@ public class SettingsActivity extends PreferenceActivity {
             Preference licenses = findPreference("licenses");
             licenses.setIntent(new Intent(getActivity(), OpenSourceLicensesActivity.class));
 
+            Preference changelog = findPreference("changelog");
+            String version = BuildConfig.VERSION_NAME;
+            if(version.contains("-")){
+                //if debug build, the version string will be like v0.1-<sha hash>
+                version = version.split("-")[0];
+            }
+            changelog.setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/the-blue-alliance/the-blue-alliance-android/releases/tag/v"+version)));
+
             Preference tbaLink = findPreference("tba_link");
             tbaLink.setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.thebluealliance.com")));
         }
