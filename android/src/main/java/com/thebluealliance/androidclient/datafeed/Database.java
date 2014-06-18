b4435ee91c676b1d210f75f27e6eba08dfdc3ad4
Bryce Matsuda <iexist4jcore@gmail.com>
2014-06-17 15:18:26 -1000

    removed unused variable that came up while thinking of ways to update db, whoops.

--- android/src/main/java/com/thebluealliance/androidclient/datafeed/Database.java
+++ android/src/main/java/com/thebluealliance/androidclient/datafeed/Database.java
@@ -28,7 +28,6 @@ import java.util.Map;
 public class Database extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 8;
-    private static final int VERSIONS_SINCE_EIGHT = 0;
     private Context context;
     public static final String DATABASE_NAME = "the-blue-alliance-android-database",
             TABLE_API = "api",
