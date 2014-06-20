15282d562cd5f19beecae7915bd09c3eb4d0f825
Nathan Walters <nwalters512@gmail.com>
2014-06-20 14:47:10 -0500

    Cleaned up imports

--- android/src/main/java/com/thebluealliance/androidclient/fragments/team/TeamMediaFragment.java
+++ android/src/main/java/com/thebluealliance/androidclient/fragments/team/TeamMediaFragment.java
@@ -1,7 +1,6 @@
 package com.thebluealliance.androidclient.fragments.team;
 
 import android.content.Context;
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
