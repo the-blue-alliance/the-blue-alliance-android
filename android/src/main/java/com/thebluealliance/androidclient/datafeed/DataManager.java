6be0bbcf48583f573ff05bac27e6d7531ca87b31
Phil Lopreiato <plnyyanks@gmail.com>
2014-06-30 14:36:56 -0400

    forogt a string format arg

--- android/src/main/java/com/thebluealliance/androidclient/datafeed/DataManager.java
+++ android/src/main/java/com/thebluealliance/androidclient/datafeed/DataManager.java
@@ -253,7 +253,7 @@ public class DataManager {
             ArrayList<Match> results = new ArrayList<>();
             Log.d("match list", "fetching matches for " + eventKey);
 
-            String apiUrl = String.format(TBAv2.API_URL.get(TBAv2.QUERY.EVENT_MATCHES));
+            String apiUrl = String.format(TBAv2.API_URL.get(TBAv2.QUERY.EVENT_MATCHES), eventKey);
             String sqlWhere = Database.Matches.EVENT + " = ?";
             APIResponse<ArrayList<Match>> matchResponse = Match.queryList(c, loadFromCache, null, sqlWhere, new String[]{eventKey}, new String[]{apiUrl});
             for (Match match: matchResponse.getData()) {
