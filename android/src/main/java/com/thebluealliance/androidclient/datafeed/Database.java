769e707463fe4685908cdfd4e969ff03f746733c
Phil Lopreiato <plnyyanks@gmail.com>
2014-07-17 19:40:03 -0400

    forgot to update db version

--- android/src/main/java/com/thebluealliance/androidclient/datafeed/Database.java
+++ android/src/main/java/com/thebluealliance/androidclient/datafeed/Database.java
@@ -35,7 +35,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Database extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 12;
+    private static final int DATABASE_VERSION = 13;
     private Context context;
     public static final String DATABASE_NAME = "the-blue-alliance-android-database",
             TABLE_API = "api",
