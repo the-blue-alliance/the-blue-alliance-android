88f2f265693e5086b41ac0bfa9c95691e418f498
Phil Lopreiato <plnyyanks@gmail.com>
2014-07-18 16:40:46 -0400

    Bug where first tab didn't get updated on tab strip update fixed

--- android/src/main/java/com/thebluealliance/androidclient/adapters/EventsByWeekFragmentPagerAdapter.java
+++ android/src/main/java/com/thebluealliance/androidclient/adapters/EventsByWeekFragmentPagerAdapter.java
@@ -1,30 +1,19 @@
 package com.thebluealliance.androidclient.adapters;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.astuetz.PagerSlidingTabStrip;
-import com.thebluealliance.androidclient.Constants;
 import com.thebluealliance.androidclient.Utilities;
-import com.thebluealliance.androidclient.activities.RefreshableHostActivity;
 import com.thebluealliance.androidclient.background.DownloadEventList;
-import com.thebluealliance.androidclient.comparators.EventWeekLabelSortComparator;
 import com.thebluealliance.androidclient.fragments.EventListFragment;
 import com.thebluealliance.androidclient.helpers.EventHelper;
-import com.thebluealliance.androidclient.interfaces.RefreshListener;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
-import java.util.Set;
 
 /**
  * Created by Nathan on 4/22/2014.
@@ -34,15 +23,18 @@ public class EventsByWeekFragmentPagerAdapter extends FragmentPagerAdapter {
     private int mCount;
     private int mYear;
     private ArrayList<String> thisYearsWeekLabels;
+    private ArrayList<EventListFragment> fragments;
     private String selectedTab;
     private PagerSlidingTabStrip tabs;
     private ViewPager pager;
+    private boolean firstTabChanged;
 
 
     public EventsByWeekFragmentPagerAdapter(Context c, FragmentManager fm, int year, PagerSlidingTabStrip tabs, ViewPager pager) {
         super(fm);
         mYear = year;
         thisYearsWeekLabels = new ArrayList<>();
+        fragments = new ArrayList<>();
         selectedTab = String.format(EventHelper.REGIONAL_LABEL, 1);
         thisYearsWeekLabels.add(selectedTab);
         this.tabs = tabs;
@@ -50,6 +42,7 @@ public class EventsByWeekFragmentPagerAdapter extends FragmentPagerAdapter {
         mCount = 1;
         DownloadEventList task = new DownloadEventList(c, this);
         task.execute(year);
+        firstTabChanged = false;
     }
 
     @Override
@@ -68,6 +61,7 @@ public class EventsByWeekFragmentPagerAdapter extends FragmentPagerAdapter {
     }
 
     public void setLabels(ArrayList<String> labels){
+        firstTabChanged = !getPageTitle(0).equals(labels.get(0));
         selectedTab = getPageTitle(pager.getCurrentItem()).toString();
         thisYearsWeekLabels = labels;
         mCount = thisYearsWeekLabels.size();
@@ -78,7 +72,9 @@ public class EventsByWeekFragmentPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return EventListFragment.newInstance(mYear, position, null, getPageTitle(position).toString());
+        EventListFragment f = EventListFragment.newInstance(mYear, position, null, getPageTitle(position).toString());
+        fragments.add(position, f);
+        return f;
     }
 
     public ArrayList<String> getLabels() {
@@ -88,5 +84,8 @@ public class EventsByWeekFragmentPagerAdapter extends FragmentPagerAdapter {
     public void notifyDataSetChanged() {
         super.notifyDataSetChanged();
         tabs.notifyDataSetChanged();
+        if(firstTabChanged){
+            fragments.get(0).updateHeader(getPageTitle(0).toString());
+        }
     }
 }
