78b375df3f993d32b5a0facda63602dce8a43adc
Phil Lopreiato <plnyyanks@gmail.com>
2014-07-27 11:05:44 -0400

    add basic GCM sending structure

---
+++ android/src/main/java/com/thebluealliance/androidclient/gcm/GCMHelper.java
@@ -0,0 +1,124 @@
+package com.thebluealliance.androidclient.gcm;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.thebluealliance.androidclient.Constants;
+import com.thebluealliance.androidclient.Utilities;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * File created by phil on 7/27/14.
+ */
+public class GCMHelper {
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String PROPERTY_GCM_REG_ID = "gcm_registration_id";
+    private static final String GCM_SENDER_FORMAT = "%s@gcm.googleapis.com";
+
+    private static GoogleCloudMessaging gcm;
+    private static String senderId;
+    private static AtomicInteger msgId = new AtomicInteger();
+
+    public static GoogleCloudMessaging getGcm(Context context){
+        return GoogleCloudMessaging.getInstance(context);
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private static boolean checkPlayServices(Activity activity) {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, activity,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(Constants.LOG_TAG, "This device is not supported.");
+                activity.finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private static String getRegistrationId(Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_GCM_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(Constants.LOG_TAG, "GCM Registration not found.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    /**
+     * If the app gets updated, we  need to clear the GCM registration ID,
+     * as the existing one is not guarateed to continue working
+     */
+    public static void clearGCMRegistration(Context context){
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putString(PROPERTY_GCM_REG_ID, "").commit();
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId registration ID
+     */
+    public static void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_GCM_REG_ID, regId);
+        editor.commit();
+    }
+
+    /**
+     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+     * or CCS to send messages to your app.
+     */
+    public static void sendRegistrationIdToBackend() {
+        // TODO implement
+        /**
+         * We want to send a secured HTTP POST request to tba.com where we tell it the following:
+         * Registration ID, OS, App Version
+         */
+    }
+
+    public static String getSenderId(Context c){
+        if(senderId == null){
+            senderId = Utilities.readLocalProperty(c, "gcm.senderId");
+        }
+        return senderId;
+    }
+
+    public static void sendUpstreamMessage(Context c, Bundle data) throws IOException{
+        String id = Integer.toString(msgId.incrementAndGet());
+        getGcm(c).send(
+                String.format(GCM_SENDER_FORMAT, getSenderId(c)),
+                id,
+                data);
+    }
+
+}
---
+++ android/src/main/java/com/thebluealliance/androidclient/gcm/RegisterGCM.java
@@ -0,0 +1,53 @@
+package com.thebluealliance.androidclient.gcm;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.thebluealliance.androidclient.Constants;
+
+import java.io.IOException;
+
+/**
+ * File created by phil on 7/27/14.
+ */
+public class RegisterGCM extends AsyncTask<Void, Void, Void> {
+
+    private Context context;
+
+    public RegisterGCM(Context c) {
+        context = c;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        String msg = "";
+        try {
+            GoogleCloudMessaging gcm = GCMHelper.getGcm(context);
+
+            String senderId = GCMHelper.getSenderId(context);
+            String regid = gcm.register(senderId);
+            msg = "Device registered, registration ID=" + regid;
+
+            // You should send the registration ID to your server over HTTP,
+            // so it can use GCM/HTTP or CCS to send messages to your app.
+            // The request to your server should be authenticated if your app
+            // is using accounts.
+            GCMHelper.sendRegistrationIdToBackend();
+
+            // For this demo: we don't need to send it because the device
+            // will send upstream messages to a server that echo back the
+            // message using the 'from' address in the message.
+
+            // Persist the regID - no need to register again.
+            GCMHelper.storeRegistrationId(context, regid);
+        } catch (IOException ex) {
+            Log.e(Constants.LOG_TAG, "Error registering gcm:" + ex.getMessage());
+            // If there is an error, don't just keep trying to register.
+            // Require the user to click a button again, or perform
+            // exponential back-off.
+        }
+        return null;
+    }
+}
