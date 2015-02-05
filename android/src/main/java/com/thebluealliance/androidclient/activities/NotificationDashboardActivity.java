8440bcea9cf7bae2edbc8440006624c2a68b80f0
Phil Lopreiato <plnyyanks@gmail.com>
2015-02-05 17:22:37 -0500

    Update dashboard strings and add help

--- android/src/main/java/com/thebluealliance/androidclient/activities/NotificationDashboardActivity.java
+++ android/src/main/java/com/thebluealliance/androidclient/activities/NotificationDashboardActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.thebluealliance.androidclient.Constants;
 import com.thebluealliance.androidclient.R;
+import com.thebluealliance.androidclient.Utilities;
 import com.thebluealliance.androidclient.fragments.NotificationDashboardFragment;
 
 /**
@@ -36,7 +39,25 @@ public class NotificationDashboardActivity extends RefreshableHostActivity {
 
     private void setupActionBar(){
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setActionBarTitle(getString(R.string.notification_dashboard_title)); 
+        setActionBarTitle(getString(R.string.notification_dashboard_title));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.notification_dash_help_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.dash_help:
+                Utilities.showHelpDialog(this, R.raw.notification_dash_help, getString(R.string.action_help));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     
     @Override
