309ea8497d5402eea110c6606783e0f9f54288cd
Phil Lopreiato <plnyyanks@gmail.com>
2015-02-20 21:54:43 -0500

    Try out FB Stetho. Seems cool
    Switches HTTP to using okhttp

--- android/src/main/java/com/thebluealliance/androidclient/TBAAndroid.java
+++ android/src/main/java/com/thebluealliance/androidclient/TBAAndroid.java
@@ -3,6 +3,7 @@ package com.thebluealliance.androidclient;
 import android.app.Application;
 import android.util.Log;
 
+import com.facebook.stetho.Stetho;
 import com.google.android.gms.analytics.Tracker;
 
 /**
@@ -14,6 +15,13 @@ public class TBAAndroid extends Application {
     public void onCreate() {
         super.onCreate();
         Log.i(Constants.LOG_TAG, "Welcome to The Blue Alliance for Android, v" + BuildConfig.VERSION_NAME);
+        if(Utilities.isDebuggable()){
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                            .build());
+        }
     }
 
     public Tracker getTracker(Analytics.GAnalyticsTracker tracker) {
