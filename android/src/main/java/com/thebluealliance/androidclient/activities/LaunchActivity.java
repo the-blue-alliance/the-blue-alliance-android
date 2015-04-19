2b99f5a183f8a00b5774db0a75b2352047c91909
Phil Lopreiato <plnyyanks@gmail.com>
2015-04-19 15:07:36 -0400

    Version bump to 2.2.0

--- android/src/main/java/com/thebluealliance/androidclient/activities/LaunchActivity.java
+++ android/src/main/java/com/thebluealliance/androidclient/activities/LaunchActivity.java
@@ -164,7 +164,6 @@ public class LaunchActivity extends Activity implements View.OnClickListener, Lo
                     case 43: //bugfix: extra 2015 CMP division. Remove its cached response so it'll get downloaded again
                         Database.getInstance(this).getResponseTable().deleteResponse("http://www.thebluealliance.com/api/v2/events/2015");
                         break;
-                    case 45:
                     case 46: //recreate search indexes to contain foreign keys
                         RecreateSearchIndexes.startActionRecreateSearchIndexes(this);
                         break;
