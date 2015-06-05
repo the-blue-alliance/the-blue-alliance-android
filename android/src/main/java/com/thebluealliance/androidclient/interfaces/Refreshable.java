e3da800828f25c89e9b35cda25a7607f8cef1f19
Nathan Walters <nwalters512@gmail.com>
2015-06-04 21:10:32 -0500

    Added new framework for refreshing; changed how multidex was done.

---
+++ android/src/main/java/com/thebluealliance/androidclient/interfaces/Refreshable.java
@@ -0,0 +1,18 @@
+package com.thebluealliance.androidclient.interfaces;
+
+/**
+ * An interface for an object that can be considered refreshable (it can be signaled to start
+ * refreshing its content).
+ * <p>
+ * Created by Nathan on 6/4/2015.
+ */
+public interface Refreshable {
+
+    /**
+     * Indicates that this object should start refreshing its content.
+     *
+     * @param requestedByUser true if this refresh was explicitly requested by the user; for
+     *                        instance, if they clicked a refresh button
+     */
+    void onRefreshStart(boolean requestedByUser);
+}
