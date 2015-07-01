67c72142ff40a05c9fef7a88209fed9417e9478a
Phil Lopreiato <plnyyanks@gmail.com>
2015-06-30 23:08:55 -0400

    Sort team lists

--- android/src/main/java/com/thebluealliance/androidclient/subscribers/TeamListSubscriber.java
+++ android/src/main/java/com/thebluealliance/androidclient/subscribers/TeamListSubscriber.java
@@ -1,28 +1,28 @@
 package com.thebluealliance.androidclient.subscribers;
 
-import android.widget.ListView;
-import android.widget.ProgressBar;
-
+import com.thebluealliance.androidclient.comparators.TeamSortByNumberComparator;
 import com.thebluealliance.androidclient.listitems.ListItem;
 import com.thebluealliance.androidclient.models.BasicModel;
 import com.thebluealliance.androidclient.models.Team;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TeamListSubscriber extends BaseAPISubscriber<List<Team>, List<ListItem>> {
 
-    public ListView mListView;
-    public ProgressBar mProgressBar;
+    private TeamSortByNumberComparator mComparator;
 
     public TeamListSubscriber() {
         super();
         mDataToBind = new ArrayList<>();
+        mComparator = new TeamSortByNumberComparator();
     }
 
     @Override
     public void parseData() throws BasicModel.FieldNotDefinedException {
         mDataToBind.clear();
+        Collections.sort(mAPIData, mComparator);
         for (int i=0; i < mAPIData.size(); i++) {
             Team team = mAPIData.get(i);
             if (team == null) {
