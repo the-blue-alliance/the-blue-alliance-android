80f6c233ecbd4021d2749210e232fa19391a060a
Phil Lopreiato <plnyyanks@gmail.com>
2015-08-05 20:37:06 -0400

    Test DistrictListSubscriber

--- android/src/test/java/com/thebluealliance/androidclient/database/DatabaseMocker.java
+++ android/src/test/java/com/thebluealliance/androidclient/database/DatabaseMocker.java
@@ -2,6 +2,7 @@ package com.thebluealliance.androidclient.database;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.thebluealliance.androidclient.database.tables.EventsTable;
 import com.thebluealliance.androidclient.database.tables.TeamsTable;
 
 import static org.mockito.Mockito.mock;
@@ -15,6 +16,12 @@ public class DatabaseMocker {
     public static TeamsTable mockTeamsTable(Database database) {
         TeamsTable table = new TeamsTable(mock(SQLiteDatabase.class));
         when(database.getTeamsTable()).thenReturn(table);
+        return table;
+    }
+
+    public static EventsTable mockEventsTable(Database database) {
+        EventsTable table = new EventsTable(mock(SQLiteDatabase.class));
+        when(database.getEventsTable()).thenReturn(table);
         return table;
     }
 
