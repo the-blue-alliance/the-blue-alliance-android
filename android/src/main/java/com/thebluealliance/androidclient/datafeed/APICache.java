68c14cd212723af41172e8661c27eb728f81ac62
Phil Lopreiato <plnyyanks@gmail.com>
2015-08-12 15:56:49 -0400

    Create Observables with Observable.create

--- android/src/main/java/com/thebluealliance/androidclient/datafeed/APICache.java
+++ android/src/main/java/com/thebluealliance/androidclient/datafeed/APICache.java
@@ -15,7 +15,6 @@ import com.thebluealliance.androidclient.database.tables.MediasTable;
 import com.thebluealliance.androidclient.database.tables.TeamsTable;
 import com.thebluealliance.androidclient.helpers.DistrictHelper;
 import com.thebluealliance.androidclient.models.Award;
-import com.thebluealliance.androidclient.models.BasicModel;
 import com.thebluealliance.androidclient.models.District;
 import com.thebluealliance.androidclient.models.DistrictTeam;
 import com.thebluealliance.androidclient.models.Event;
@@ -58,48 +57,85 @@ public class APICache implements APIv2 {
                 observer.onError(e);
             }
         });
-        //return Observable.just(mDb.getTeamsTable().getForQuery(null, where, new String[]{Integer.toString(startNum), Integer.toString(startNum + 499)}));
     }
 
     @Override
     public Observable<Team> fetchTeam(String teamKey) {
-        Team team = mDb.getTeamsTable().get(teamKey);
-        return Observable.just(team);
+        return Observable.create((observer) -> {
+            try {
+                Team team = mDb.getTeamsTable().get(teamKey);
+                observer.onNext(team);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Event>> fetchTeamEvents(String teamKey, int year) {
-        List<Event> events = mDb.getEventTeamsTable().getEvents(teamKey, year);
-        return Observable.just(events);
+        return Observable.create((observer) -> {
+            try {
+                List<Event> events = mDb.getEventTeamsTable().getEvents(teamKey, year);
+                observer.onNext(events);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Award>> fetchTeamAtEventAwards(String teamKey, String eventKey) {
-        return Observable.just(mDb.getAwardsTable().getTeamAtEventAwards(teamKey, eventKey));
+        return Observable.create((observer) -> {
+            try {
+                observer.onNext(mDb.getAwardsTable().getTeamAtEventAwards(teamKey, eventKey));
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Match>> fetchTeamAtEventMatches(String teamKey, String eventKey) {
-        return Observable.just(mDb.getMatchesTable().getTeamAtEventMatches(teamKey, eventKey));
+        return Observable.create((observer) -> {
+            try {
+                observer.onNext(mDb.getMatchesTable().getTeamAtEventMatches(teamKey, eventKey));
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<JsonArray> fetchTeamYearsParticipated(String teamKey) {
-        Team team = mDb.getTeamsTable().get(teamKey);
-        JsonArray years;
-        try {
-            years = team == null ? new JsonArray() : team.getYearsParticipated();
-        } catch (BasicModel.FieldNotDefinedException e) {
-            years = new JsonArray();
-        }
-        return Observable.just(years);
+        return Observable.create((observer) -> {
+            try {
+                Team team = mDb.getTeamsTable().get(teamKey);
+                observer.onNext(team == null ? new JsonArray() : team.getYearsParticipated());
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Media>> fetchTeamMediaInYear(String teamKey, int year) {
-        String where = MediasTable.TEAMKEY + " = ? AND " + MediasTable.YEAR + " = ?";
-        return Observable.just(mDb.getMediasTable().getForQuery(null, where, new String[]{teamKey,
-                Integer.toString(year)}));
+        return Observable.create((observer) -> {
+            try {
+                String where = MediasTable.TEAMKEY + " = ? AND " + MediasTable.YEAR + " = ?";
+                List<Media> medias =
+                  mDb.getMediasTable()
+                    .getForQuery(null, where, new String[]{teamKey, Integer.toString(year)});
+                observer.onNext(medias);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
@@ -114,10 +150,11 @@ public class APICache implements APIv2 {
 
     @Override
     public Observable<List<Event>> fetchEventsInYear(int year) {
-        String where = String.format("%1$s = ?", EventsTable.YEAR);
         return Observable.create((observer) -> {
             try {
-                List<Event> events = mDb.getEventsTable().getForQuery(null, where, new String[]{Integer.toString(year)});
+                String where = String.format("%1$s = ?", EventsTable.YEAR);
+                List<Event> events = mDb.getEventsTable()
+                  .getForQuery(null, where, new String[]{Integer.toString(year)});
                 if (!observer.isUnsubscribed()) {
                     observer.onNext(events);
                     observer.onCompleted();
@@ -126,139 +163,236 @@ public class APICache implements APIv2 {
                 observer.onError(e);
             }
         });
-        //return Observable.just(mDb.getEventsTable().getForQuery(null, where, new String[]{Integer.toString(year)}));
     }
 
     public Observable<List<Event>> fetchEventsInWeek(int year, int week) {
-        String where =
-                String.format("%1$s = ? AND %2$s = ?", EventsTable.YEAR, EventsTable.WEEK);
-        return Observable.just(mDb.getEventsTable()
-                .getForQuery(null, where, new String[]{Integer.toString(year), Integer.toString(week)}));
+        return Observable.create((observer) -> {
+            try {
+                String where =
+                  String.format("%1$s = ? AND %2$s = ?", EventsTable.YEAR, EventsTable.WEEK);
+                List<Event> events = mDb.getEventsTable()
+                  .getForQuery(null, where,
+                    new String[]{Integer.toString(year), Integer.toString(week)});
+                observer.onNext(events);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     public Observable<List<Event>> fetchEventsInMonth(int year, int month) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(year, month, 1);
-        String start = Long.toString(cal.getTimeInMillis());
-        cal.add(Calendar.MONTH, 1);
-        cal.add(Calendar.DAY_OF_MONTH, -1);
-        String end = Long.toString(cal.getTimeInMillis());
+        return Observable.create((observer) -> {
+            try {
+                Calendar cal = Calendar.getInstance();
+                cal.set(year, month, 1);
+                String start = Long.toString(cal.getTimeInMillis());
+                cal.add(Calendar.MONTH, 1);
+                cal.add(Calendar.DAY_OF_MONTH, -1);
+                String end = Long.toString(cal.getTimeInMillis());
 
-        String where = String.format(
-                "%1$s >= ? AND %2$s < ?",
-                EventsTable.START,
-                EventsTable.END);
-        return Observable.just(mDb.getEventsTable()
-                .getForQuery(null, where, new String[]{start, end}));
+                String where = String.format(
+                  "%1$s >= ? AND %2$s < ?",
+                  EventsTable.START,
+                  EventsTable.END);
+                List<Event> events = mDb.getEventsTable()
+                  .getForQuery(null, where, new String[]{start, end});
+                observer.onNext(events);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<Event> fetchEvent(String eventKey) {
-        Event event = mDb.getEventsTable().get(eventKey);
-        return Observable.just(event);
+        return Observable.create((observer) -> {
+            try {
+                Event event = mDb.getEventsTable().get(eventKey);
+                observer.onNext(event);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Team>> fetchEventTeams(String eventKey) {
-        List<Team> teams = mDb.getEventTeamsTable().getTeams(eventKey);
-        return Observable.just(teams);
+        return Observable.create((observer) -> {
+            try {
+                List<Team> teams = mDb.getEventTeamsTable().getTeams(eventKey);
+                observer.onNext(teams);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<JsonArray> fetchEventRankings(String eventKey) {
-        try {
-            Event event = mDb.getEventsTable()
-                    .get(eventKey, new String[]{EventsTable.RANKINGS});
-            if (event != null) {
-                return Observable.just(event.getRankings());
+        return Observable.create((observer) -> {
+            try {
+                Event event = mDb.getEventsTable()
+                  .get(eventKey, new String[]{EventsTable.RANKINGS});
+                observer.onNext(event != null ? event.getRankings() : null);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
             }
-        } catch (BasicModel.FieldNotDefinedException e) {
-
-        }
-        return Observable.just(null);
+        });
     }
 
     @Override
     public Observable<List<Match>> fetchEventMatches(String eventKey) {
-        String where = String.format("%1$s = ?", MatchesTable.EVENT);
-        return Observable.just(
-                mDb.getMatchesTable().getForQuery(null, where, new String[]{eventKey}));
+        return Observable.create((observer) -> {
+            try {
+                String where = String.format("%1$s = ?", MatchesTable.EVENT);
+                List<Match> matches = mDb.getMatchesTable()
+                  .getForQuery(null, where, new String[]{eventKey});
+                observer.onNext(matches);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<JsonObject> fetchEventStats(String eventKey) {
-        try {
-            Event event = mDb.getEventsTable()
-                    .get(eventKey, new String[]{EventsTable.STATS});
-            if (event != null) {
-                return Observable.just(event.getStats());
+        return Observable.create((observer) -> {
+            try {
+                Event event = mDb.getEventsTable()
+                  .get(eventKey, new String[]{EventsTable.STATS});
+                observer.onNext(event != null ? event.getStats() : null);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
             }
-        } catch (BasicModel.FieldNotDefinedException e) {
-
-        }
-        return Observable.just(null);
+        });
     }
 
     @Override
     public Observable<List<Award>> fetchEventAwards(String eventKey) {
-        String where = String.format("%1$s = ?", AwardsTable.EVENTKEY);
-        return Observable.just(
-                mDb.getAwardsTable().getForQuery(null, where, new String[]{eventKey}));
+        return Observable.create((observer) -> {
+            try {
+                String where = String.format("%1$s = ?", AwardsTable.EVENTKEY);
+                List<Award> awards = mDb.getAwardsTable()
+                  .getForQuery(null, where, new String[]{eventKey});
+                observer.onNext(awards);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<JsonObject> fetchEventDistrictPoints(String eventKey) {
-        Event event = mDb.getEventsTable().get(eventKey);
-        try {
-            return Observable.just(event.getDistrictPoints());
-        } catch (BasicModel.FieldNotDefinedException e) {
-            return Observable.just(null);
-        }
+        return Observable.create((observer) -> {
+            try {
+                Event event = mDb.getEventsTable().get(eventKey);
+                observer.onNext(event != null ? event.getDistrictPoints() : null);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<District>> fetchDistrictList(int year) {
-        String where = String.format("%1$s = ?", DistrictsTable.YEAR);
-        return Observable.just(
-                mDb.getDistrictsTable().getForQuery(null, where, new String[]{Integer.toString(year)}));
+        return Observable.create((observer) -> {
+            try {
+                String where = String.format("%1$s = ?", DistrictsTable.YEAR);
+                List<District> districts = mDb.getDistrictsTable()
+                  .getForQuery(null, where, new String[]{Integer.toString(year)});
+                observer.onNext(districts);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<Event>> fetchDistrictEvents(String districtShort, int year) {
-        String where =
-                String.format("$1%s = ? AND %2$s = ?", EventsTable.YEAR, EventsTable.DISTRICT);
-        int districtEnum = DistrictHelper.DISTRICTS.fromAbbreviation(districtShort).ordinal();
-        return Observable.just(mDb.getEventsTable().getForQuery(
-                null,
-                where,
-                new String[]{Integer.toString(year), Integer.toString(districtEnum)}));
+        return Observable.create((observer) -> {
+            try {
+                String where =
+                  String.format("$1%s = ? AND %2$s = ?", EventsTable.YEAR, EventsTable.DISTRICT);
+                int districtEnum = DistrictHelper.DISTRICTS.fromAbbreviation(districtShort).ordinal();
+                List<Event> events = mDb.getEventsTable().getForQuery(
+                  null,
+                  where,
+                  new String[]{Integer.toString(year), Integer.toString(districtEnum)});
+                observer.onNext(events);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<List<DistrictTeam>> fetchDistrictRankings(String districtShort, int year) {
-        String where = String.format(
-                "%1$s = ? AND %2$s = ?",
-                DistrictTeamsTable.YEAR,
-                DistrictTeamsTable.DISTRICT_ENUM);
-        int districtEnum = DistrictHelper.DISTRICTS.fromAbbreviation(districtShort).ordinal();
-        return Observable.just(mDb.getDistrictTeamsTable().getForQuery(
-                null,
-                where,
-                new String[]{Integer.toString(year), Integer.toString(districtEnum)}));
+        return Observable.create((observer) -> {
+            try {
+                String where = String.format(
+                  "%1$s = ? AND %2$s = ?",
+                  DistrictTeamsTable.YEAR,
+                  DistrictTeamsTable.DISTRICT_ENUM);
+                int districtEnum = DistrictHelper.DISTRICTS.fromAbbreviation(districtShort).ordinal();
+                List<DistrictTeam> districtTeams = mDb.getDistrictTeamsTable().getForQuery(
+                  null,
+                  where,
+                  new String[]{Integer.toString(year), Integer.toString(districtEnum)});
+                observer.onNext(districtTeams);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     @Override
     public Observable<Match> fetchMatch(String matchKey) {
-        return Observable.just(mDb.getMatchesTable().get(matchKey));
+        return Observable.create((observer) -> {
+            try {
+                Match match = mDb.getMatchesTable().get(matchKey);
+                observer.onNext(match);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     public Observable<List<Subscription>> fetchUserSubscriptions(Context context) {
-        String account = AccountHelper.getSelectedAccount(context);
-        return Observable.just(mDb.getSubscriptionsTable().getForUser(account));
+        return Observable.create((observer) -> {
+            try {
+                String account = AccountHelper.getSelectedAccount(context);
+                List<Subscription> subscriptions = mDb.getSubscriptionsTable().getForUser(account);
+                observer.onNext(subscriptions);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 
     public Observable<List<Favorite>> fetchUserFavorites(Context context) {
-        String account = AccountHelper.getSelectedAccount(context);
-        return Observable.just(mDb.getFavoritesTable().getForUser(account));
+        return Observable.create((observer) -> {
+            try {
+                String account = AccountHelper.getSelectedAccount(context);
+                List<Favorite> favorites = mDb.getFavoritesTable().getForUser(account);
+                observer.onNext(favorites);
+                observer.onCompleted();
+            } catch (Exception e) {
+                observer.onError(e);
+            }
+        });
     }
 }
