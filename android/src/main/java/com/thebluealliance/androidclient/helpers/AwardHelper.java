ad6d7f9a4d3591dc5d0af2726f42d091ea540b8f
Phil Lopreiato <plnyyanks@gmail.com>
2015-12-04 13:37:24 -0500

    Add AwardHelperTest

--- android/src/main/java/com/thebluealliance/androidclient/helpers/AwardHelper.java
+++ android/src/main/java/com/thebluealliance/androidclient/helpers/AwardHelper.java
@@ -2,21 +2,10 @@ package com.thebluealliance.androidclient.helpers;
 
 import com.thebluealliance.androidclient.Utilities;
 
-/**
- * Created by phil on 6/24/14.
- */
 public class AwardHelper {
-    public static String buildWinnerString(String awardee, String team) {
-        if (awardee.isEmpty()) {
-            return "" + team;
-        } else if (team.isEmpty()) {
-            return awardee;
-        } else {
-            return awardee + " (" + team + ")";
-        }
-    }
 
     public static boolean validateAwardKey(String key) {
+        if (key == null) return false;
         String[] split = key.split(":");
         return split.length == 2 &&
                 EventHelper.validateEventKey(split[0]) &&
