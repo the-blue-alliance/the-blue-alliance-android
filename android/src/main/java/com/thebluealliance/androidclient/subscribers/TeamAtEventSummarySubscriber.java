the-blue-alliance/the-blue-alliance-android

commit 1ab5f7f883cbec2300bfc4347749415f643be53f

Team@Event Summary shows as much as it can
This means it isn't totally dependent on the incoming EventBus for
things to show. This means we'll have data to display when the
Observable completes, so the no data view won't always be shown on first
load.

Fixes #495

android/src/main/java/com/thebluealliance/androidclient/subscribers/TeamAtEventSummarySubscriber.java
@@ -125,6 +125,15 @@ public class TeamAtEventSummarySubscriber extends BaseAPISubscriber<Model, List<
             rankingString = EventHelper.createRankingBreakdown(rankingElements);
         }
 
+        // Rank
+        if (rank > 0) {
+            mDataToBind.add(new LabelValueListItem(
+              mResources.getString(R.string.team_at_event_rank),
+              rank + Utilities.getOrdinalFor(rank)));
+        }
+
+        LabelValueListItem rankBreakdownItem = new LabelValueListItem("Ranking Breakdown", rankingString);
+
         MatchHelper.EventStatus status;
         try {
             status = MatchHelper.evaluateStatusOfTeam(event, mMatches, mTeamKey);
@@ -135,12 +144,7 @@ public class TeamAtEventSummarySubscriber extends BaseAPISubscriber<Model, List<
         }
 
         if (status != MatchHelper.EventStatus.NOT_AVAILABLE) {
-            // Rank
-            if (rank > 0) {
-                mDataToBind.add(new LabelValueListItem(
-                  mResources.getString(R.string.team_at_event_rank),
-                  rank + Utilities.getOrdinalFor(rank)));
-            }
+
             // Record
             /* Don't show for 2015 events, because no wins and such */
             if (year != 2015 && !recordString.equals("0-0-0")) {
@@ -169,7 +173,7 @@ public class TeamAtEventSummarySubscriber extends BaseAPISubscriber<Model, List<
 
             // Ranking Breakdown
             if (rankingString != null && !rankingString.isEmpty()) {
-                mDataToBind.add(new LabelValueListItem("Ranking Breakdown", rankingString));
+                mDataToBind.add(rankBreakdownItem);
             }
 
             if (lastMatch != null) {
@@ -182,9 +186,11 @@ public class TeamAtEventSummarySubscriber extends BaseAPISubscriber<Model, List<
                   mResources.getString(R.string.title_next_match),
                   mMatchRenderer.renderFromModel(nextMatch, MatchRenderer.RENDER_DEFAULT)));
             }
-
-            mDataToBind.add(new EmptyListElement(""));
+        } else {
+            mDataToBind.add(rankBreakdownItem);
         }
+
+        mDataToBind.add(new EmptyListElement(""));
     }
 
     /**

commit 4bcba1324800fafbde0cc2a042bb5a0b8446998b

Fix bugs introduced in 1ab5f7f
Issues where Teasm@Event Fragment was showing things/not binding
NoData when there was no data available. Rel #495

android/src/main/java/com/thebluealliance/androidclient/subscribers/TeamAtEventSummarySubscriber.java
@@ -186,11 +186,17 @@ public class TeamAtEventSummarySubscriber extends BaseAPISubscriber<Model, List<
                   mResources.getString(R.string.title_next_match),
                   mMatchRenderer.renderFromModel(nextMatch, MatchRenderer.RENDER_DEFAULT)));
             }
-        } else {
+        } else if (rank > 0) {
+            // Only show ranking breakdown if rankings are available
             mDataToBind.add(rankBreakdownItem);
         }
 
-        mDataToBind.add(new EmptyListElement(""));
+        if (mDataToBind.size() > 0) {
+            // If there is data to add, then add an empty item next to it so we can scroll
+            // all the way down and not have the FAB overlap with anything
+            mDataToBind.add(new EmptyListElement(""));
+        }
+
     }
 
     /**