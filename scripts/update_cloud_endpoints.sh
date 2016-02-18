f8ed7ed77da8e349a66539e3c3a767294ee6a324
Phil Lopreiato <plnyyanks@gmail.com>
2016-02-18 15:15:41 -0500

    Add script to generate Google Cloud Endpoints lib
    It will place the java source of the library into the git tree

---
+++ scripts/update_cloud_endpoints.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+# Script to generate code for Cloud Endpoints client lib
+# Usage: ./scripts/update_cloud_endpoints.sh -t <path-to-tba> -s <path-to-gae-sdk> -a <debug-app-id>
+
+# Default to packaging for prod
+TBA_APP_ID="tbatv-prod-hrd"
+
+while getopts ":t:s:a:" opt; do
+  case $opt in
+    t) TBA_HOME="$OPTARG" && echo "Setting TBA Home to $TBA_HOME"
+    ;;
+    p) GAE_HOME="$OPTARG" && echo "Setting GAE Home to $GAE_HOME"
+    ;;
+    a) TBA_APP_ID="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2 && exit -1
+    ;;
+  esac
+done
+
+echo "Building Cloud Endpoints library for TBA App ID $TBA_APP_ID"
+
+# This script should be run from the root of the TBA Android Repo
+TBA_ANDROID_HOME=$(pwd)
+if [ $(basename $TBA_ANDROID_HOME) = "scripts" ]; then
+    echo "Please run from the TBA Android root directory"
+    exit -1
+fi
+
+# Get directory of TBA code. Default to $TBA_HOME environment variable, path paramter otherwise
+if [ -z "$TBA_HOME" ]; then
+    echo "Please export TBA_HOME to your environment with the path to the-blue-alliance repo, or pass it as the first paramter"
+    exit -1
+fi
+
+# Get directory of GAE SDK. Default to $GAE_HOME environment variable, path paramter otherwise
+if [ -z "$TBA_HOME" ]; then
+    echo "Please export GAE_HOME to your environment with the path to Google App Engine SDK, or pass it as the second paramter"
+    exit -1
+fi
+
+# Set up & generate endpoints client library
+cd $TBA_HOME
+
+# Set the proper app id and modify mobile API file to remove sitevar references
+perl -pi -e "s/tbatv-dev-hrd/$TBA_APP_ID/g" *.yaml
+git apply $TBA_ANDROID_HOME/scripts/endpoints.patch
+
+# Actually build library
+$GAE_HOME/endpointscfg.py get_client_lib java -o $TBA_ANDROID_HOME -bs gradle mobile_main.MobileAPI
+$RES=$?
+
+# Undo our changes
+git apply -R $TBA_ANDROID_HOME/scripts/endpoints.patch
+perl -pi -e "s/$TBA_APP_ID/tbatv-dev-hrd/g" *.yaml
+cd $TBA_ANDROID_HOME
+
+if [ $RES -neq 0 ]; then
+    echo "Endpoint generation failed"
+    echo $RES
+fi
+
+unzip -o tbaMobile-v*.zip
+rm -rf tbaMobile-v*.zip
