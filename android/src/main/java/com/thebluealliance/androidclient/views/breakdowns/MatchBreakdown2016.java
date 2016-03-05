00d2286df090ccf38f191a833d1ef4ec6c015cc2
Phil Lopreiato <plnyyanks@gmail.com>
2016-03-05 15:17:01 -0500

    Add layout for 2016 breakdown

---
+++ android/src/main/java/com/thebluealliance/androidclient/views/breakdowns/MatchBreakdown2016.java
@@ -0,0 +1,11 @@
+package com.thebluealliance.androidclient.views.breakdowns;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+public class MatchBreakdown2016 extends FrameLayout {
+
+    public MatchBreakdown2016(Context context) {
+        super(context);
+    }
+}
