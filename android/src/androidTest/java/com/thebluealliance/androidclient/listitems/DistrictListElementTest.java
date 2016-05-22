ffa35b4efc79437f2d723d6feeb628bdcfb2e68f
Phil Lopreiato <plnyyanks@gmail.com>
2016-05-22 12:48:15 -0400

    Add screenshot tests for DistrictListElement

---
+++ android/src/androidTest/java/com/thebluealliance/androidclient/listitems/DistrictListElementTest.java
@@ -0,0 +1,54 @@
+package com.thebluealliance.androidclient.listitems;
+
+import com.facebook.testing.screenshot.Screenshot;
+import com.facebook.testing.screenshot.ViewHelpers;
+import com.thebluealliance.androidclient.models.BasicModel;
+import com.thebluealliance.androidclient.models.District;
+import com.thebluealliance.androidclient.types.DistrictType;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class DistrictListElementTest extends InstrumentationTestCase {
+
+    public static final int WIDTH_DP = 400;
+
+    private static final District DISTRICT = new District();
+    static {
+        DISTRICT.setAbbreviation("ne");
+        DISTRICT.setEnum(DistrictType.NEW_ENGLAND.ordinal());
+        DISTRICT.setName("New England");
+        DISTRICT.setYear(2016);
+        DISTRICT.setKey("2016ne");
+    }
+
+    public void testRenderWithMyTba() throws BasicModel.FieldNotDefinedException {
+        View view = getView(DISTRICT, 4, true);
+        ViewHelpers.setupView(view)
+                   .setExactWidthDp(WIDTH_DP)
+                   .layout();
+
+        Screenshot.snap(view)
+                  .record();
+    }
+
+    public void testRenderWithoutMyTba() throws BasicModel.FieldNotDefinedException {
+        View view = getView(DISTRICT, 4, false);
+        ViewHelpers.setupView(view)
+                   .setExactWidthDp(WIDTH_DP)
+                   .layout();
+
+        Screenshot.snap(view)
+                  .record();
+    }
+
+    private View getView(District district, int numEvents, boolean showMyTba)
+    throws BasicModel.FieldNotDefinedException {
+        DistrictListElement element = new DistrictListElement(district, numEvents, showMyTba);
+        Context targetContext = getInstrumentation().getTargetContext();
+        LayoutInflater inflater = LayoutInflater.from(targetContext);
+        return element.getView(targetContext, inflater, null);
+    }
+}
