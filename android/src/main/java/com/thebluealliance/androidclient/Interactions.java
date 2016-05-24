ca15d7c3e5ba42f364ce29927f7636f4251d6f87
Nathan Walters <nwalters512@gmail.com>
2016-05-23 21:27:06 -0500

    Use RecyclerView for event team list (#742)
    * Use recyclerview for event team list
    
    * Removed obsolete test

--- android/src/main/java/com/thebluealliance/androidclient/Interactions.java
+++ android/src/main/java/com/thebluealliance/androidclient/Interactions.java
@@ -2,7 +2,11 @@ package com.thebluealliance.androidclient;
 
 public class Interactions {
 
+    /** EVENTS **/
     public static final int EVENT_CLICKED = 1000;
+
+    /** TEAMS **/
+    public static final int TEAM_ITEM_CLICKED = 2000;
 
     /** RANKINGS **/
     public static final int TEAM_RANKING_CLICKED = 3000;
