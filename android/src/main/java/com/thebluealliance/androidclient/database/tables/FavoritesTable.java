e54f2e71db1fdd9f2b07455ec9546902eb19c723
Nathan Walters <nwalters512@gmail.com>
2016-06-16 18:53:45 -0500

    Refactor FavoritesTable to use SqlBrite

--- android/src/main/java/com/thebluealliance/androidclient/database/tables/FavoritesTable.java
+++ android/src/main/java/com/thebluealliance/androidclient/database/tables/FavoritesTable.java
@@ -1,15 +1,19 @@
 package com.thebluealliance.androidclient.database.tables;
 
 import com.squareup.sqlbrite.BriteDatabase;
+import com.squareup.sqlbrite.SqlBrite;
 import com.thebluealliance.androidclient.database.Database;
 import com.thebluealliance.androidclient.database.ModelInflater;
 import com.thebluealliance.androidclient.models.Favorite;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import rx.Observable;
 
 public class FavoritesTable {
     public static final String KEY = "key",
@@ -27,27 +31,25 @@ public class FavoritesTable {
 
     public long add(Favorite in) {
         if (!exists(in.getKey())) {
-            return mDb.insert(Database.TABLE_FAVORITES, null, in.getParams());
+            return mBriteDb.insert(Database.TABLE_FAVORITES, in.getParams());
         }
         return -1;
     }
 
     public void add(List<Favorite> in) {
-        mDb.beginTransaction();
+        BriteDatabase.Transaction transaction = mBriteDb.newTransaction();
         try {
             for (Favorite favorite : in) {
-                if (!unsafeExists(favorite.getKey())) {
-                    mDb.insert(Database.TABLE_FAVORITES, null, favorite.getParams());
-                }
+                add(favorite);
             }
-            mDb.setTransactionSuccessful();
+            transaction.markSuccessful();
         } finally {
-            mDb.endTransaction();
+            transaction.end();
         }
     }
 
     public void remove(String key) {
-        mDb.delete(Database.TABLE_FAVORITES, KEY + " = ?", new String[]{key});
+        mBriteDb.delete(Database.TABLE_FAVORITES, KEY + " = ?", key);
     }
 
     public boolean exists(String key) {
@@ -70,21 +72,28 @@ public class FavoritesTable {
         return null;
     }
 
-    public boolean unsafeExists(String key) {
-        Cursor cursor = mDb.query(Database.TABLE_FAVORITES, null, KEY + " = ?", new String[]{key}, null, null, null, null);
-        boolean result;
-        if (cursor != null) {
-            result = cursor.moveToFirst();
-            cursor.close();
-        } else {
-            result = false;
-        }
-        return result;
+    public Observable<Favorite> getObservable(String key) {
+        String sql = SQLiteQueryBuilder.buildQueryString(
+                true,
+                Database.TABLE_FAVORITES,
+                null,
+                KEY + " = ?",
+                null,
+                null,
+                null,
+                null);
+        return mBriteDb.createQuery(Database.TABLE_FAVORITES, sql, key).map((query) -> {
+            Cursor cursor = query.run();
+            if (cursor != null && cursor.moveToFirst()) {
+                return ModelInflater.inflateFavorite(cursor);
+            }
+            return null;
+        });
     }
 
-    public ArrayList<Favorite> getForUser(String user) {
+    public List<Favorite> getForUser(String user) {
         Cursor cursor = mDb.query(Database.TABLE_FAVORITES, null, USER_NAME + " = ?", new String[]{user}, null, null, MODEL_ENUM + " ASC", null);
-        ArrayList<Favorite> favorites = new ArrayList<>();
+        List<Favorite> favorites = new ArrayList<>();
         if (cursor != null && cursor.moveToFirst()) {
             do {
                 favorites.add(ModelInflater.inflateFavorite(cursor));
@@ -93,7 +102,29 @@ public class FavoritesTable {
         return favorites;
     }
 
+    public Observable<List<Favorite>> getObservableForUser(String user) {
+        String sql = SQLiteQueryBuilder.buildQueryString(
+                true,
+                Database.TABLE_FAVORITES,
+                null,
+                USER_NAME + " = ?",
+                null,
+                null,
+                MODEL_ENUM + " ASC",
+                null);
+        return mBriteDb.createQuery(Database.TABLE_FAVORITES, sql, user).map((query) -> {
+            Cursor cursor = query.run();
+            List<Favorite> favorites = new ArrayList<>();
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    favorites.add(ModelInflater.inflateFavorite(cursor));
+                } while (cursor.moveToNext());
+            }
+            return favorites;
+        });
+    }
+
     public void recreate(String user) {
-        mDb.delete(Database.TABLE_FAVORITES, USER_NAME + " = ?", new String[]{user});
+        mBriteDb.delete(Database.TABLE_FAVORITES, USER_NAME + " = ?", user);
     }
 }
