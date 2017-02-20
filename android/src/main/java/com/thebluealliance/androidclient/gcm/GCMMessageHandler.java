1e4c60321ddc2f2eaa703c2bec7ea3e5bb548ca5
Phil Lopreiato <plnyyanks@gmail.com>
2017-02-19 21:44:29 -0500

    Update remote config on SYNC_STATUS gcm push

--- android/src/main/java/com/thebluealliance/androidclient/gcm/GCMMessageHandler.java
+++ android/src/main/java/com/thebluealliance/androidclient/gcm/GCMMessageHandler.java
@@ -7,6 +7,7 @@ import com.thebluealliance.androidclient.R;
 import com.thebluealliance.androidclient.TBAAndroid;
 import com.thebluealliance.androidclient.TbaLogger;
 import com.thebluealliance.androidclient.accounts.AccountController;
+import com.thebluealliance.androidclient.config.AppConfig;
 import com.thebluealliance.androidclient.database.Database;
 import com.thebluealliance.androidclient.database.DatabaseWriter;
 import com.thebluealliance.androidclient.database.tables.FavoritesTable;
@@ -79,6 +80,7 @@ public class GCMMessageHandler extends IntentService implements FollowsChecker {
     @Inject MatchRenderer mMatchRenderer;
     @Inject Database mDb;
     @Inject AccountController mAccountController;
+    @Inject AppConfig mAppConfig;
 
     private NotificationComponent mComponenet;
 
@@ -191,6 +193,8 @@ public class GCMMessageHandler extends IntentService implements FollowsChecker {
                 case NotificationTypes.SYNC_STATUS:
                     TbaLogger.i("Updating TBA API Status via push notification");
                     mStatusController.scheduleStatusUpdate(c);
+                    TbaLogger.i("Updating FirebaseRemoteConfig via push notification");
+                    mAppConfig.updateRemoteData();
                     break;
             }
 
