97e5fe966dcd963b4549aed0fb9e067dbfef2712
Phil Lopreiato <phil-lopreiato@users.noreply.github.com>
2017-02-22 11:51:32 -0500

    Show 2017 Event Insights (#813)
    * Refactor 2016 match insights to be more generic
    
    * Break out commonly used event insight strings
    
    * Consolidate some insights rendering code
    
    * Share code for high score
    
    * Render 2017 Event Insights

---
+++ android/src/main/java/com/thebluealliance/androidclient/renderers/insights/EventInsights2016Renderer.java
@@ -0,0 +1,66 @@
+package com.thebluealliance.androidclient.renderers.insights;
+
+import com.google.gson.JsonObject;
+
+import com.thebluealliance.androidclient.R;
+import com.thebluealliance.androidclient.listitems.EventTypeHeader;
+import com.thebluealliance.androidclient.listitems.ListItem;
+
+import android.content.res.Resources;
+import android.support.annotation.StringRes;
+
+import java.util.List;
+
+public class EventInsights2016Renderer extends EventInsightsRenderer {
+
+    public EventInsights2016Renderer(List<ListItem> eventStats, Resources resources) {
+        super(eventStats, resources);
+    }
+
+    @Override
+    void generateYearSpecificInsights(JsonObject qualData, JsonObject elimData) {
+        generate2016MatchInsights(qualData, elimData);
+        generate2016DefenseInsights(qualData, elimData);
+        generate2016TowerInsights(qualData, elimData);
+    }
+
+    private @StringRes int[] matchTitles = {R.string.breakdown_avg_low_goal, R.string
+            .breakdown_avg_high_goal, R.string.breakdown_avg_score, R.string
+            .breakdown_avg_win_score, R.string.breakdown_avg_win_margin, R.string
+            .breakdown_avg_auto_score, R.string.breakdown2016_teleop_cross, R.string
+            .breakdown2016_avg_boulder_score, R.string.breakdown2016_avg_tower_score, R.string
+            .breakdown_avg_foul_score};
+    private String[] matchKeys = {"average_low_goals", "average_high_goals", "average_score",
+            "average_win_score", "average_win_margin", "average_auto_score",
+            "average_crossing_score", "average_boulder_score", "average_tower_score",
+            "average_foul_score"};
+
+    private void generate2016MatchInsights(JsonObject quals, JsonObject elims) {
+        mEventStats.add(new EventTypeHeader(mResources.getString(R.string.breakdown_match_stats)));
+
+        addHighScore(quals, elims, "high_score");
+        addQualVsElimInsights(quals, elims, matchTitles, matchKeys);
+    }
+
+    private String[] defenseTitle = {"LowBar", "A_ChevalDeFrise", "A_Portcullis", "B_Ramparts",
+            "B_Moat", "C_SallyPort", "C_Drawbridge", "D_RoughTerrain", "D_RockWall", "breaches"};
+    private @StringRes int[] defenseName = {R.string.defense2016_low_bar, R.string
+            .defense2016_cdf, R.string.defense2016_portcullis, R.string.defense2016_ramparts, R
+            .string.defense2016_moat, R.string.defense2016_sally_port, R.string
+            .defense2016_drawbridge, R.string.defense2016_rough_terrain, R.string
+            .defense2016_rock_wall, R.string.defense2016_breaches};
+
+    private void generate2016DefenseInsights(JsonObject quals, JsonObject elims) {
+        mEventStats.add(new EventTypeHeader(mResources.getString(R.string.breakdown2016_defense_stats)));
+        addQualVsElimInsightsWithPercentage(quals, elims, defenseName, defenseTitle);
+    }
+
+    private @StringRes int[] towerTitles = {R.string.breakdown2016_challenge, R.string
+            .breakdown2016_scales, R.string.breakdown2016_captures};
+    private String[] towerKeys = {"challenges", "scales", "captures"};
+
+    private void generate2016TowerInsights(JsonObject quals, JsonObject elims) {
+        mEventStats.add(new EventTypeHeader(mResources.getString(R.string.breakdown2016_tower_stats)));
+        addQualVsElimInsightsWithPercentage(quals, elims, towerTitles, towerKeys);
+    }
+}
---
+++ android/src/main/java/com/thebluealliance/androidclient/renderers/insights/EventInsights2017Renderer.java
@@ -0,0 +1,59 @@
+package com.thebluealliance.androidclient.renderers.insights;
+
+import com.google.gson.JsonObject;
+
+import com.thebluealliance.androidclient.R;
+import com.thebluealliance.androidclient.listitems.EventTypeHeader;
+import com.thebluealliance.androidclient.listitems.ListItem;
+
+import android.content.res.Resources;
+import android.support.annotation.StringRes;
+
+import java.util.List;
+
+public class EventInsights2017Renderer extends EventInsightsRenderer {
+
+
+    public EventInsights2017Renderer(List<ListItem> eventStats, Resources resources) {
+        super(eventStats, resources);
+    }
+
+    @Override
+    void generateYearSpecificInsights(JsonObject quals, JsonObject elims) {
+        generate2017MatchInsights(quals, elims);
+        generate2017BonusInsights(quals, elims);
+    }
+
+    /* Strings for basic match insights */
+    private static @StringRes int[] matchTitles = {R.string.breakdown_avg_score,
+            R.string.breakdown_avg_win_score, R.string.breakdown_avg_win_margin,
+            R.string.breakdown2017_average_mobility, R.string.breakdown2017_average_rotor,
+            R.string.breakdown2017_average_fuel, R.string.breakdown_avg_high_goal,
+            R.string.breakdown_avg_low_goal, R.string.breakdown2017_average_takeoff,
+            R.string.breakdown_avg_foul_score};
+    private static String[] matchKeys = {"average_score", "average_win_score", "average_win_margin",
+            "average_mobility_points_auto", "average_rotor_points", "average_fuel_points",
+            "average_high_goals", "average_low_goals", "average_takeoff_points_teleop",
+            "average_foul_score"};
+
+    private void generate2017MatchInsights(JsonObject quals, JsonObject elims) {
+        mEventStats.add(new EventTypeHeader(mResources.getString(R.string.breakdown_match_stats)));
+
+        addHighScore(quals, elims, "high_score");
+        addQualVsElimInsights(quals, elims, matchTitles, matchKeys);
+    }
+
+    private static @StringRes int[] bonusTitles = {R.string.breakdown2017_auto_mobility_points,
+            R.string.breakdown2017_teleop_takeoff, R.string.breakdown2017_pressure_bonus,
+            R.string.breakdown2017_rotor1_auto, R.string.breakdown2017_rotor2_auto,
+            R.string.breakdown2017_rotor1, R.string.breakdown2017_rotor2,
+            R.string.breakdown2017_rotor3, R.string.breakdown2017_rotor4};
+    private static String[] bonusKeys = {"mobility_counts", "takeoff_counts", "kpa_achieved",
+            "rotor_1_engaged_auto", "rotor_1_engaged_auto", "rotor_1_engaged", "rotor_2_engaged",
+            "rotor_3_engaged", "rotor_4_engaged"};
+
+    private void generate2017BonusInsights(JsonObject quals, JsonObject elims) {
+        mEventStats.add(new EventTypeHeader(mResources.getString(R.string.breakdown2017_bonus_stats)));
+        addQualVsElimInsightsWithPercentage(quals, elims, bonusTitles, bonusKeys);
+    }
+}
