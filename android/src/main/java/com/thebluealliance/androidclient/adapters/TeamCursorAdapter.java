d043c2ca221eca53e7d5c55432645f82967bba8a
Phil Lopreiato <plnyyanks@gmail.com>
2017-03-07 16:27:55 -0500

    Null check TeamCursorAdapter names

--- android/src/main/java/com/thebluealliance/androidclient/adapters/TeamCursorAdapter.java
+++ android/src/main/java/com/thebluealliance/androidclient/adapters/TeamCursorAdapter.java
@@ -41,7 +41,8 @@ import android.widget.TextView;
         int teamNumber = cursor.getInt(2);
         String teamName = cursor.getString(4);
         ((TextView) view.findViewById(R.id.team_number)).setText(cursor.getString(2));
-        ((TextView) view.findViewById(R.id.team_name)).setText(teamName.isEmpty() ? "Team " + teamNumber : teamName);
+        ((TextView) view.findViewById(R.id.team_name)).setText(teamName == null || teamName.isEmpty()
+                                                               ? "Team " + teamNumber : teamName);
         ((TextView) view.findViewById(R.id.team_location)).setText(cursor.getString(5));
         view.findViewById(R.id.team_info).setVisibility(View.GONE);
     }
