a97e0397716975d5c425544dac320453131f3cd3
Phil Lopreiato <plnyyanks@gmail.com>
2019-02-18 19:28:53 -0500

    update release signing to work with lazy task configuration

--- scripts/travis/travis-worker.sh
+++ scripts/travis/travis-worker.sh
@@ -41,9 +41,9 @@ case "$1" in
         # Move local.properties and tba.properties to proper location
         mv config/local.properties.ci local.properties
 
-	# Generate a key (with the default debug creds) to use for this test build
-	keytool -genkey -v -keystore /home/travis/.android/debug.keystore -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug,O=Android,C=US"
-        ./gradlew assembleRelease
+    	# Generate a key (with the default debug creds) to use for this test build
+	    keytool -genkey -v -keystore /home/travis/.android/debug.keystore -keyalg RSA -keysize 2048 -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug,O=Android,C=US"
+        ./gradlew assembleRelease --stacktrace
         ;;
 
     "SCREENSHOT")
