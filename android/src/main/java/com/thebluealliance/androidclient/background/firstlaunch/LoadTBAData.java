bdbb02bfc1d8103ed7189865babd2ded5057752c
Phil Lopreiato <plnyyanks@gmail.com>
2020-02-22 12:26:31 -0500

    lint

--- android/src/main/java/com/thebluealliance/androidclient/background/firstlaunch/LoadTBAData.java
+++ android/src/main/java/com/thebluealliance/androidclient/background/firstlaunch/LoadTBAData.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import retrofit2.Call;
 import retrofit2.Response;
