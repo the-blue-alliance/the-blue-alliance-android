3a74bd464dff500ed30d9f2e9f49966710af4f76
Phil Lopreiato <plnyyanks@gmail.com>
2020-03-01 15:08:15 -0500

    add in noop stub for tests

---
+++ android/src/test/java/com/crashlytics/android/Crashlytics.java
@@ -0,0 +1,15 @@
+package com.crashlytics.android;
+
+public class Crashlytics {
+    public static void log(String message) {
+        // noop
+    }
+
+    public static void logcat(int leve, String tag, String message) {
+        // noop
+    }
+
+    public static void logException(Throwable throwable) {
+        // noop
+    }
+}
