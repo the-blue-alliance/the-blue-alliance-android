8f96e3f84a893c07e16902f47db0d36e1626f19e
Phil Lopreiato <plnyyanks@gmail.com>
2020-03-08 12:21:37 -0400

    more

--- android/src/test/java/com/thebluealliance/androidclient/gcm/GCMMessageHandlerTest.java
+++ android/src/test/java/com/thebluealliance/androidclient/gcm/GCMMessageHandlerTest.java
@@ -4,8 +4,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+
 import com.google.gson.JsonObject;
 import com.thebluealliance.androidclient.datafeed.framework.ModelMaker;
+import com.thebluealliance.androidclient.gcm.notifications.BaseNotification;
 import com.thebluealliance.androidclient.gcm.notifications.NotificationTypes;
 
 import org.junit.Before;
@@ -32,8 +34,12 @@ public class GCMMessageHandlerTest {
 
         private NotificationManager mNotificationManager;
 
-        @ParameterizedRobolectricTestRunner.Parameter(0) public String mNotificationType;
-        @ParameterizedRobolectricTestRunner.Parameter(1) public String mNotificationDataFileName;
+        @ParameterizedRobolectricTestRunner.Parameter(0)
+        public String mNotificationType;
+        @ParameterizedRobolectricTestRunner.Parameter(1)
+        public String mNotificationDataFileName;
+        @ParameterizedRobolectricTestRunner.Parameter(2)
+        public int mExpectedPriority;
 
         @Before
         public void setUp() {
@@ -44,35 +50,39 @@ public class GCMMessageHandlerTest {
         @ParameterizedRobolectricTestRunner.Parameters(name = "NotificationType = {0}")
         public static Collection<Object[]> data() {
             return Arrays.asList(new Object[][]{
-                    {NotificationTypes.ALLIANCE_SELECTION, "notification_alliance_selection"},
-                    {NotificationTypes.AWARDS, "notification_awards_posted"},
-                    {NotificationTypes.DISTRICT_POINTS_UPDATED, "notification_district_points_updated"},
-                    {NotificationTypes.EVENT_DOWN, "notification_event_down"},
-                    {NotificationTypes.LEVEL_STARTING, "notification_level_starting"},
-                    {NotificationTypes.MATCH_SCORE, "notification_match_score"},
-                    {NotificationTypes.PING, "notification_ping"},
-                    {NotificationTypes.SCHEDULE_UPDATED, "notification_schedule_updated"},
-                    {NotificationTypes.UPCOMING_MATCH, "notification_upcoming_match"},
+                    {NotificationTypes.ALLIANCE_SELECTION, "notification_alliance_selection", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.AWARDS, "notification_awards_posted", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.DISTRICT_POINTS_UPDATED, "notification_district_points_updated", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.EVENT_DOWN, "notification_event_down", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.LEVEL_STARTING, "notification_level_starting", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.MATCH_SCORE, "notification_match_score", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.PING, "notification_ping", Notification.PRIORITY_LOW},
+                    {NotificationTypes.SCHEDULE_UPDATED, "notification_schedule_updated", Notification.PRIORITY_HIGH},
+                    {NotificationTypes.UPCOMING_MATCH, "notification_upcoming_match", Notification.PRIORITY_HIGH},
             });
         }
 
         @Test
-        public void testPostNotification() {
-            Intent intent = buildIntent();
+        public void testPostSingleNotification() {
+            Intent intent = buildIntent(mNotificationType, mNotificationDataFileName);
             GCMMessageHandlerWithMocks service = Robolectric.setupService(GCMMessageHandlerWithMocks.class);
             service.onCreate();
             service.onHandleWork(intent);
 
-            List<Notification> notification = Shadows.shadowOf(mNotificationManager).getAllNotifications();
-            assertEquals(1, notification.size());
-        }
+            List<Notification> notifications = Shadows.shadowOf(mNotificationManager).getAllNotifications();
+            assertEquals(1, notifications.size());
 
-        private Intent buildIntent() {
-            JsonObject notificationData = ModelMaker.getModel(JsonObject.class, mNotificationDataFileName);
-            Intent intent = new Intent("com.google.android.c2dm.intent.RECEIVE");
-            intent.putExtra("notification_type", mNotificationType);
-            intent.putExtra("message_data", notificationData.toString());
-            return intent;
+            Notification notification = notifications.get(0);
+            assertEquals(BaseNotification.NOTIFICATION_CHANNEL, notification.getChannelId());
+            assertEquals(mExpectedPriority, notification.priority);
         }
+    }
+
+    private static Intent buildIntent(String notificationType, String dataFileName) {
+        JsonObject notificationData = ModelMaker.getModel(JsonObject.class, dataFileName);
+        Intent intent = new Intent("com.google.android.c2dm.intent.RECEIVE");
+        intent.putExtra("notification_type", notificationType);
+        intent.putExtra("message_data", notificationData.toString());
+        return intent;
     }
 }
