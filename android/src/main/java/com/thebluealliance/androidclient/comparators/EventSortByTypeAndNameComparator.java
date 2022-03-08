65cb959f73e344e9790427da52366e6bec6f9bec
Phil Lopreiato <plnyyanks@gmail.com>
2022-03-08 06:33:14 -0500

    Improve null safety in EventSortByTypeAndNameComparator

--- android/src/main/java/com/thebluealliance/androidclient/comparators/EventSortByTypeAndNameComparator.java
+++ android/src/main/java/com/thebluealliance/androidclient/comparators/EventSortByTypeAndNameComparator.java
@@ -10,8 +10,16 @@ public class EventSortByTypeAndNameComparator implements Comparator<Event> {
     public int compare(Event event, Event event2) {
         // Preseason < regional < district < district_cmp < cmp_division < cmp_finals < offseason
         if (event.getEventTypeEnum() == event2.getEventTypeEnum()) {
-            int districtSort = event.getDistrictKey().compareTo(event2.getDistrictKey());
-            int nameSort = event.getShortName().compareTo(event2.getShortName());
+            int districtSort = 0;
+            if (event.getDistrictKey() != null && event2.getDistrictKey() != null) {
+                districtSort = event.getDistrictKey().compareTo(event2.getDistrictKey());
+            }
+
+            int nameSort = 0;
+            if (event.getShortName() != null && event2.getShortName() != null) {
+                nameSort = event.getShortName().compareTo(event2.getShortName());
+            }
+
             if (districtSort == 0) {
                 return nameSort;
             } else {
@@ -21,7 +29,10 @@ public class EventSortByTypeAndNameComparator implements Comparator<Event> {
             EventType type1 = event.getEventTypeEnum();
             EventType type2 = event2.getEventTypeEnum();
             int typeCompare = type1.getSortOrder() - type2.getSortOrder();
-            if (typeCompare == 0 && event.getEventTypeEnum() == EventType.DISTRICT) {
+            if (typeCompare == 0
+                    && event.getEventTypeEnum() == EventType.DISTRICT
+                    && event.getDistrictKey() != null
+                    && event2.getDistrictKey() != null) {
                 return event.getDistrictKey().compareTo(event2.getDistrictKey());
             } else {
                 return typeCompare;
