8ccf6d5458a0903178967dc30c4ba7f2f25ffa23
Bryan Herbst <tanis.7x@gmail.com>
2022-03-27 06:06:54 -0500

    Fix geo: intents on Android 11+ (#963)
    Instead of querying for packages (which now requires <queries> entries in the manfiest, just catch ActivityNotFoundException

--- android/src/main/java/com/thebluealliance/androidclient/listeners/SocialClickListener.java
+++ android/src/main/java/com/thebluealliance/androidclient/listeners/SocialClickListener.java
@@ -1,16 +1,13 @@
 package com.thebluealliance.androidclient.listeners;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.view.View;
 import android.widget.Toast;
 
 import com.thebluealliance.androidclient.helpers.AnalyticsHelper;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -30,23 +27,18 @@ public class SocialClickListener implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        PackageManager manager = mContext.getPackageManager();
         if (view.getTag() != null) {
-
             String uri = view.getTag().toString();
 
             //social button was clicked. Track the call
             AnalyticsHelper.sendSocialUpdate(mContext, uri, mModelKey);
 
             Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
-            List<ResolveInfo> handlers = manager.queryIntentActivities(i, 0);
-            if (!handlers.isEmpty()) {
-                // There is an application to handle this intent intent
+            try {
                 mContext.startActivity(i);
-            } else {
-                // No application can handle this intent
+            } catch (ActivityNotFoundException e) {
                 Toast.makeText(mContext, "No app can handle that request", Toast.LENGTH_SHORT)
-                  .show();
+                        .show();
             }
         }
     }
