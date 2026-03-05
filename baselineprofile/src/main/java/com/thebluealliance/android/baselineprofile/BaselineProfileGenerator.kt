29cdd7e931942f2384166886f9513f96b31b6488
Bryan Herbst <bryan.c.herbst@gmail.com>
2026-03-05 06:23:08 -0600

    Generate startup and baseline profiles during release (#1138)
    * Generate startup and baseline profiles during release
    
    * Fix package for baselineprofile tests
    
    * Remove extra newline
    
    * One more round of cleanup

---
+++ baselineprofile/src/main/java/com/thebluealliance/android/baselineprofile/BaselineProfileGenerator.kt
@@ -0,0 +1,68 @@
+package com.thebluealliance.android.baselineprofile
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.textAsString
+import androidx.test.uiautomator.uiAutomator
+import androidx.test.uiautomator.waitForStable
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * This test class generates a basic startup baseline profile for the target package.
+ *
+ *
+ * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
+ * the equivalent `generateBaselineProfile` gradle task:
+ * ```
+ * ./gradlew :app:generateReleaseBaselineProfile
+ * ```
+ * The run configuration runs the Gradle task and applies filtering to run only the generators.
+ *
+ * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
+ **/
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BaselineProfileGenerator {
+
+  @get:Rule
+  val rule = BaselineProfileRule()
+
+  @Test
+  fun generate() {
+    // The application id for the running build variant is read from the instrumentation arguments.
+    rule.collect(
+      packageName = InstrumentationRegistry.getArguments().getString("targetAppId")
+        ?: throw Exception("targetAppId not passed as instrumentation runner arg"),
+
+      // See: https://d.android.com/topic/performance/baselineprofiles/dex-layout-optimizations
+      includeInStartupProfile = true
+    ) {
+      // This block defines the app's critical user journey. Here we are interested in
+      // optimizing for app startup. But you can also navigate and scroll through your most important UI.
+
+      // Start default activity for your app
+      pressHome()
+      startActivityAndWait()
+
+      uiAutomator {
+          // Go to teams tab, wait for team 1 to be displayed
+          onElement { textAsString() == "Teams" }.click()
+          onElement { textAsString()?.contains("1") == true }
+
+          // Go to Districts tab, wait for California to be displayed
+          onElement { textAsString() == "Districts" }.click()
+          onElement { textAsString()?.contains("California") == true }
+
+          // Click search icon, type "10" (which will have both teams and events), wait for results
+          onElement { contentDescription == "Search" }.click()
+          onElement { isFocused }.setText("10")
+          // Match on team 100, because "10" will also be in the search box. This waits for actual results
+          onElement { textAsString()?.contains("100") == true }.waitForStable()
+      }
+    }
+  }
+}
