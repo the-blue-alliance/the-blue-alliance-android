d36e9dc920bfc1dcd74cab835ef894f647521c0a
Phil Lopreiato <plnyyanks@gmail.com>
2026-04-29 05:49:17 -0400

    bump room version

--- app/src/main/kotlin/com/thebluealliance/android/data/local/TBADatabase.kt
+++ app/src/main/kotlin/com/thebluealliance/android/data/local/TBADatabase.kt
@@ -60,7 +60,7 @@ import com.thebluealliance.android.data.local.entity.TeamEventStatusEntity
         EventRankingSortOrderEntity::class,
         TeamEventStatusEntity::class,
     ],
-    version = 14,
+    version = 15,
     exportSchema = false,
 )
 abstract class TBADatabase : RoomDatabase() {
